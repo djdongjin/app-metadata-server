59e411eb4457b54e284a087b5b82cd80c8a6be66
Jin Dong <djdongjin95@gmail.com>
2021-11-14 22:25:49 -0800

    refactor apiserver

--- metadataserver/apiserver.go
+++ metadataserver/apiserver.go
@@ -1,13 +1,14 @@
 package metadataserver
 
 import (
-	"fmt"
+	"net/http"
 
-	// "github.com/djdongjin/app-metadata-server/common"
 	"github.com/djdongjin/app-metadata-server/persisters"
 	"github.com/gin-gonic/gin"
 )
 
+// ApiServer is the main struct handling HTTP requests. It holds a DataStore that
+// implements the Persister interface.
 type ApiServer struct {
 	DataStore persisters.Persister
 }
@@ -20,6 +21,8 @@ func (server *ApiServer) PersistMetadata(c *gin.Context) {
 
 	metadata, err := YamlStringToMetadata(string(reqBody))
 	_ = server.DataStore.Persist(metadata)
+
+	c.String(http.StatusCreated, "Metadata persisted.")
 }
 
 func (server *ApiServer) RetrieveMetadata(c *gin.Context) {
@@ -28,25 +31,31 @@ func (server *ApiServer) RetrieveMetadata(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(string(queryBody))
+	allMetadata := server.DataStore.Retrieve(string(queryBody))
+	var res string
+	for _, md := range allMetadata {
+		res += MetadataToYamlString(md) + "\n\n"
+	}
+
+	c.String(http.StatusOK, res)
 }
 
 func (server *ApiServer) GetMetadata(c *gin.Context) {
 	title := c.Param("title")
 
 	if metadata, ok := server.DataStore.Get(title); ok {
-		fmt.Println(MetadataToYamlString(metadata))
+		c.String(http.StatusOK, MetadataToYamlString(metadata))
 	} else {
-		fmt.Println("no Metadata found")
+		c.String(http.StatusNotFound, "Metadata not found.")
 	}
 }
 
 func (server *ApiServer) DeleteMetadata(c *gin.Context) {
 	title := c.Param("title")
 
-	if metadata, ok := server.DataStore.Get(title); ok {
-		fmt.Println(MetadataToYamlString(metadata))
+	if metadata, ok := server.DataStore.Delete(title); ok {
+		c.String(http.StatusOK, "Medadata deleted: \n\n"+MetadataToYamlString(metadata))
 	} else {
-		fmt.Println("no Metadata found")
+		c.String(http.StatusNotFound, "Metadata not found.")
 	}
 }
