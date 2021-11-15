0a5058962c274dc4e953df56a7ca5af4d418047b
Jin Dong <djdongjin95@gmail.com>
2021-11-14 22:25:34 -0800

    complete persisters

---
+++ persisters/inmemory_persister.go
@@ -0,0 +1,47 @@
+package persisters
+
+import (
+	"fmt"
+
+	"github.com/djdongjin/app-metadata-server/common"
+)
+
+// InMemoryPersister holds a map to store metadata where the key is title
+// and value is corresponding Metadata.
+type InMemoryPersister struct {
+	data map[string]common.Metadata
+}
+
+func (p InMemoryPersister) Persist(metadata common.Metadata) error {
+	key := metadata.Title
+	p.data[key] = metadata
+
+	return nil
+}
+
+func (p InMemoryPersister) Retrieve(query string) []common.Metadata {
+	// TODO: add filtering logic.
+	res := make([]common.Metadata, 0)
+	for _, v := range p.data {
+		res = append(res, v)
+	}
+
+	return res
+}
+
+func (p InMemoryPersister) Get(title string) (common.Metadata, bool) {
+	metadata, ok := p.data[title]
+
+	return metadata, ok
+}
+
+func (p InMemoryPersister) Delete(title string) (common.Metadata, bool) {
+	if metadata, ok := p.data[title]; ok {
+		fmt.Println(len(p.data))
+		delete(p.data, title)
+		fmt.Println(len(p.data))
+		return metadata, ok
+	} else {
+		return common.Metadata{}, false
+	}
+}
--- persisters/persister.go
+++ persisters/persister.go
@@ -6,6 +6,7 @@ import (
 	"github.com/djdongjin/app-metadata-server/common"
 )
 
+// Persister is the interface for storing/retrieving Metadata.
 type Persister interface {
 	Persist(common.Metadata) error
 	Retrieve(query string) []common.Metadata
@@ -13,36 +14,7 @@ type Persister interface {
 	Delete(title string) (common.Metadata, bool)
 }
 
-type InMemoryPersister struct {
-	data map[string]common.Metadata
-}
-
-func (p InMemoryPersister) Persist(metadata common.Metadata) error {
-	key := metadata.Title
-	p.data[key] = metadata
-
-	return nil
-}
-
-func (p InMemoryPersister) Retrieve(query string) []common.Metadata {
-	return make([]common.Metadata, 0)
-}
-
-func (p InMemoryPersister) Get(title string) (common.Metadata, bool) {
-	metadata, ok := p.data[title]
-
-	return metadata, ok
-}
-
-func (p InMemoryPersister) Delete(title string) (common.Metadata, bool) {
-	if metadata, ok := p.data[title]; ok {
-		delete(p.data, title)
-		return metadata, ok
-	} else {
-		return common.Metadata{}, false
-	}
-}
-
+// New is the factory function that returns a Persister implementation.
 func New(tp string) (Persister, error) {
 	switch tp {
 	case "InMemoryPersister":
